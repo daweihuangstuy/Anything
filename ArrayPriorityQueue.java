55bd699b402d1a47522811555188e7c5d97b2fda
Karina Ionkina <Karina@Karinas-MacBook-Air.local>
2017-04-25 00:37:51 -0400

    fully working version c:

--- ArrayPriorityQueue.java
+++ ArrayPriorityQueue.java
@@ -1,88 +1,98 @@
 import java.util.ArrayList;
 public class ArrayPriorityQueue implements PriorityQueue<Ticket>{
-    
+
     private ArrayList<Ticket> q;
     public ArrayPriorityQueue(){
-	q = new ArrayList();
-    }    
-    
+        q = new ArrayList();
+    }
 
     public void add(Ticket x)
-   { 
-	//initialize high, low, midpt indices
-	int lo = 0;
-	int med = 0;
-	int hi = q.size() - 1;
+   {
+       if (isEmpty()) {q.add(0, x); return;}
 
-	while ( lo < hi ) { //running until target is found or bounds cross
+       if(q.size() == 1){
+           if (q.get(0).compareTo(x) == 0 || q.get(0).compareTo(x) > 0){
+               q.add(1,x);
+               return; }
+           else {
+               q.add(0, x);
+               return;}
+       }
 
-	    med = (lo + hi) / 2;
-	    int a = q.get(med).compareTo(x);
-	    if ( a == 0 ) {
-		//equal value found, insert here
-		lo = med;
-		break;
-	    }
-	    else if ( a > 0 )
-		//newVal < med, so look at lower half of data
-		hi = med - 1;
-	    else
-		//newVal > med, so look at upper half of data
-		lo = med + 1;
-	}
-	//	System.out.println(lo);
-	//System.out.println(q.size());
-	//keeps order
-	if(lo != 0){
-	    while(q.get(lo - 1).compareTo(q.get(lo)) == 0){
+
+        //initialize high, low, midpt indices                                                                                     
+        int lo = 0;
+        int med = 0;
+        int hi = q.size() - 1;
+
+
+        while ( lo <= hi) { //running until target is found or bounds cross                                                         
+            med = (lo + hi) / 2;
+            int a = x.compareTo(q.get(med));
+            if ( a == 0){
+                lo = med;
+                hi = lo;
+            }
+            if ( a > 0 ){
+                //newVal less than val at med so we move closer to the front                                                       
+                hi = med - 1;}
+            else{
+		         //newVal less than val at med so we move closer to the back
+		lo = med + 1; }
+        }
+        //keeps order                                                                                                              
+	if(lo > 0){
+            while(q.get(lo - 1).compareTo(x) == 0){
            lo -= 1;
-	    }
-	}
-       
-	// If you made it this far, newVal is not present.
-	// So insert at lo.
-	q.add(lo,x);
-	
-    }//end add
+            }
+            }
 
+        // If you made it this far, newVal is not present.                                                // So insert at lo.                                                                                                        
+        q.add(lo,x);
+    }//end add                                                                                                                     
 
     //returns whether or not the PQ is empty
     public boolean isEmpty(){
-	return q.size() == 0;
+        return q.size() == 0;
     }
 
-    //Returns the smallest item in the PQ from the front
-    public Ticket peekMin(){
-	return q.get(0);
+    //Returns the smallest item in the PQ from the front  
+ public Ticket peekMin(){
+        return q.get(0);
     }
 
-    //Removes and returns the smallest item from the front
+    //Removes and returns the smallest item from the front                                                                         
     public Ticket removeMin(){
-	Ticket old = peekMin();
-	q.remove(0);
-	return old;
+        Ticket old = peekMin();
+        q.remove(0);
+        return old;
     }
+   
+
     public String toString(){
-	String ret = "";
-	for (int i = q.size() - 1; i >= 0; i--){
-	    ret += q.get(i).getID() + " --> ";
-	}
+        String ret = " FRONT ... ";
+        for (int i = q.size() - 1; i >= 0; i--){
+            ret += " --> " + q.get(i).getID();
+        }
+    ret+=" ...END";
     return ret;
-    }
-    
+    } 
+
+
+
+
     public static void main(String[] args){
-	ArrayPriorityQueue a = new ArrayPriorityQueue();
-	Ticket alpha = new Ticket(209853738, 0);
-	Ticket beta = new Ticket(1234, 15);
-	Ticket king = new Ticket(999, 0);
+        ArrayPriorityQueue a = new ArrayPriorityQueue();
+        Ticket alpha = new Ticket(209853738, 0);
+        Ticket beta = new Ticket(1234, 15);
+        Ticket king = new Ticket(999, 0);
 
-	a.add(beta);
-	System.out.println(a);
-	a.add(alpha);
-	System.out.println(a);
-	a.add(king);
-	System.out.println(a);
+        a.add(beta);
+        System.out.println(a);
+        a.add(alpha);
+        System.out.println(a);
+        a.add(king);
+        System.out.println(a);
     }
-    
-
 }
+
--- HelpDesk.java
+++ HelpDesk.java
@@ -1,15 +1,38 @@
 public class HelpDesk{
- 
-   //the Queue of tickets...
-    private ArrayPriorityQueue<Ticket> TQ;
 
-   //this will hold the processed ticket's ID number
+   //the Queue of tickets...                                                                        
+    private ArrayPriorityQueue TQ;
+
+   //this will hold the processed ticket's ID number                                                
     private int ticket;
- 
-    
+
     public HelpDesk(){
-	ticket = TQ.peekMin().getID();
+        ticket = TQ.peekMin().getID();
+    }
+
+    public static void main(String[] args){
+
+        ArrayPriorityQueue a = new ArrayPriorityQueue();
+        Ticket alpha = new Ticket(1, 1);
+        Ticket beta = new Ticket(15, 15);
+        Ticket king = new Ticket(0, 0);
+        Ticket dan = new Ticket(0, 0);
+        Ticket alice = new Ticket(6, 6);
+        Ticket bob = new Ticket(12, 12);
+        a.add(beta);
+        System.out.println(a);
+	System.out.println(alpha.compareTo(beta)); //-1
+        a.add(alpha);
+        System.out.println(a);
+        a.add(king);
+        System.out.println(a);
+        a.add(bob);
+        a.add(dan);
+        a.add(alice);
+        System.out.println(a);
     }
 
 
+
 }
+
